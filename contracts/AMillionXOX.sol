b357c8f665264a6c856bc870db2da80f4a12a867
Kingsley Victor <javaprodigy56@gmail.com>
2021-08-17 18:12:38 +0100

    update contract

--- contracts/AMillionXOX.sol
+++ contracts/AMillionXOX.sol
@@ -5,17 +5,55 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract AMillionXOX is ERC20 {
   uint public INITIAL_SUPPLY = 1000000 * 10 ** 18;
+  address private contractCreator;
+  uint private locked = 0;
+
+  modifier onlyCreator {
+    require(msg.sender == contractCreator, "Only contract creator can execute this operation.");
+    _;
+  }
 
   mapping(address => uint) private balances;
   mapping(address => mapping(address => uint)) private allowed;
 
   constructor() public ERC20("One Million XOX", "XOX") {
-    balances[msg.sender] = INITIAL_SUPPLY;
+    contractCreator = msg.sender;
+    lock(400000 * 10 ** 18);
+    balances[contractCreator] = INITIAL_SUPPLY - locked;
+
     emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+  }
+
+  function lock(uint amount) public onlyCreator returns (bool) {
+    
+    if (INITIAL_SUPPLY > 0 && locked == 0) {
+      require(INITIAL_SUPPLY > amount, "Cannot lock all token");
+    }
+    if (locked > 0) {
+      require((locked + amount) < totalSupply(), "Amount to be locked + currently locked must be less than total supply");
+    }
+    if (balances[contractCreator] > 0) {
+      require(balances[contractCreator] > amount, "Amount to be locked must be less than balance of contract creator");
+      balances[contractCreator] = SafeMath.sub(balances[contractCreator], amount);
+    }
+
+    locked = SafeMath.add(locked, amount);
+    return true;
+  }
+
+  function unlock(uint amount) public onlyCreator returns (bool) {
+    require(locked >= amount, "Currently locked amount must be greater than or equal to amount to be unlocked");
+    locked = SafeMath.sub(locked, amount);
+    balances[contractCreator] = SafeMath.add(balances[contractCreator], amount);
+    return true;
   }
 
   function decimals() public view virtual override returns (uint8) {
     return 18;
+  }
+
+  function totalLocked() public view returns (uint) {
+    return locked;
   }
 
   function totalSupply() public view virtual override returns (uint) {
